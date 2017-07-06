43341bb9962d1472f13266e7a6691e494598be3c
jtcy0123 <jasmin0123@hotmail.com>
2017-07-06 10:43:51 +0800

    ajax to display error msg

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ApplicationRecord
   include Clearance::User
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :password, length: { in: 6..10 }
 end
