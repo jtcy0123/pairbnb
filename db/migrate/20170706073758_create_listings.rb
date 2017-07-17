4c1211b10f4847f8ef6a604777f3e092ce291ef9
jtcy0123 <jasmin0123@hotmail.com>
2017-07-17 14:44:23 +0800

    add.

--- db/migrate/20170706073758_create_listings.rb
+++ db/migrate/20170706073758_create_listings.rb
@@ -3,7 +3,6 @@ class CreateListings < ActiveRecord::Migration[5.0]
     create_table :listings do |t|
       t.string :name
       t.string :location
-      t.string :city
       t.integer :pax_num
       t.integer :price
       t.references :user
