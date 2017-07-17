37ccf42f73429073a93c78b4fcb0818fd3eed1d0
jtcy0123 <jasmin0123@hotmail.com>
2017-07-17 14:56:47 +0800

    .

--- db/migrate/20170713095131_add_columns.rb
+++ db/migrate/20170713095131_add_columns.rb
@@ -1,5 +1,5 @@
 class AddColumns < ActiveRecord::Migration[5.0]
   def change
-    add_column :listings, :status, default: "unverified"
+    add_column :listings, :status, :string, default: "unverified"
   end
 end
