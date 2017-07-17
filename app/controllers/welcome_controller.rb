78baf0a5b74272ce05596c8b7d6c28fc9e36281d
jtcy0123 <jasmin0123@hotmail.com>
2017-07-17 11:35:04 +0800

    modify welcome page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,17 +3,21 @@ class WelcomeController < ApplicationController
   LISTINGS_PER_PAGE = 8
 
   def index
-    if params[:tag]
-      @listings = Listing.tagged_with(params[:tag]).where(status: "verified").order('created_at DESC').limit(LISTINGS_PER_PAGE).offset(@page * LISTINGS_PER_PAGE)
-      @lists = Listing.tagged_with(params[:tag]).where(status: "verified")
-    else
-      if signed_in? && current_user.role == "admin"
-        @listings = Listing.where(status: "unverified").order('created_at DESC').limit(LISTINGS_PER_PAGE).offset(@page * LISTINGS_PER_PAGE)
-        @lists = Listing.where(status: "unverified")
+    if Listing.all.length != 0
+      if params[:tag]
+        @listings = Listing.tagged_with(params[:tag]).where(status: "verified").order('created_at DESC').limit(LISTINGS_PER_PAGE).offset(@page * LISTINGS_PER_PAGE)
+        @lists = Listing.tagged_with(params[:tag]).where(status: "verified")
       else
-        @listings = Listing.where(status: "verified").order('created_at DESC').limit(LISTINGS_PER_PAGE).offset(@page * LISTINGS_PER_PAGE)
-        @lists = Listing.where(status: "verified")
+        if signed_in? && current_user.role == "admin"
+          @listings = Listing.where(status: "unverified").order('created_at DESC').limit(LISTINGS_PER_PAGE).offset(@page * LISTINGS_PER_PAGE)
+          @lists = Listing.where(status: "unverified")
+        else
+          @listings = Listing.where(status: "verified").order('created_at DESC').limit(LISTINGS_PER_PAGE).offset(@page * LISTINGS_PER_PAGE)
+          @lists = Listing.where(status: "verified")
+        end
       end
+    else
+      @listings = Listing.all
     end
   end
 
